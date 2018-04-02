99d474c02179f067bd641524f6ba8ca44d45d086
marcio <avante.td@gmail.com>
2018-04-02 05:55:25 -0300

    Templates modificados para exibir posts do banco de dados

--- blog/models.py
+++ blog/models.py
@@ -4,10 +4,10 @@ from django.utils import timezone
 # Create your models here.
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    title = models.CharField('Título', max_length=200)
+    text = models.TextField('Texto')
+    created_date = models.DateTimeField('Data de criação', default=timezone.now)
+    published_date = models.DateTimeField('Data de publicação', blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
