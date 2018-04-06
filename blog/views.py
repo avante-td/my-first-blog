50a6b79c9f7cb59e7920bb140884ef2e2a27a48f
avante-td <avante.td@gmail.com>
2018-04-06 05:24:25 -0300

    Experimentando detalhe

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
-#from django.http import HttpResponse
+from django.http import HttpResponse
 
 # Create your views here.
 def post_list(request):
     #return (HttpResponse("Hello world."))
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return (render(request, 'blog/post_list.html', {'posts': posts}))
+
+def post_detail(request, pk):
+    #post = Post.objects.get(pk=pk)
+    #post = get_object_or_404(Post, pk=pk)
+    #return render(request, 'blog/post_detail.html', {'post': post})
+    return render(HttpResponse("Hello world."))
